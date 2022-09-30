2d0251dab20167a84e7918ade7ccae8082829268
William Desportes <williamdes@wdes.fr>
2022-09-30 11:03:30 +0200

    Debug empty config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -eux
 
+ls -lah /etc/vigil.cfg
+cat /etc/vigil.cfg
+
 echo "Env names defined"
 printenv | cut -d '=' -f 1
 
@@ -7,5 +10,6 @@ echo "Replacing ENVs"
 envsubst < /etc/vigil.cfg > /etc/vigil.cfg
 
 echo "Running vigil"
+ls -lah /etc/vigil.cfg
 cat /etc/vigil.cfg
 vigil -c /etc/vigil.cfg
