89c55a3b24baf48a0ef834eeec22b7c5660ba4e5
William Desportes <williamdes@wdes.fr>
2022-09-30 10:43:50 +0200

    Try to debug run failure

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -eux
 
+echo "Env names defined"
+printenv | cut -d '=' -f 1
+
+echo "Replacing ENVs"
 envsubst < /etc/vigil.cfg > /etc/vigil.cfg
 
+echo "Running vigil"
 vigil -c /etc/vigil.cfg
