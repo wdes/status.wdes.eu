7c3a5733813bcc67b437182ec209b3ef34fac0be
William Desportes <williamdes@wdes.fr>
2022-09-30 11:15:22 +0200

    Do not overrite the deploy file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,9 +8,9 @@ printenv | cut -d '=' -f 1
 
 echo "Replacing ENVs"
 envsubst < /etc/vigil.cfg
-envsubst < /etc/vigil.cfg > /etc/vigil.cfg
+envsubst < /etc/vigil.cfg > /tmp/vigil.cfg
 
 echo "Running vigil"
-ls -lah /etc/vigil.cfg
-cat /etc/vigil.cfg
-vigil -c /etc/vigil.cfg
+ls -lah /tmp/vigil.cfg
+cat /tmp/vigil.cfg
+vigil -c /tmp/vigil.cfg
